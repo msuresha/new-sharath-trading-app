c41afda08754e3018346119766dde182669f7961
msuresha <m.suresha@gmail.com>
2024-12-18 03:53:13 +0530

    Add main App component

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import DhanTradingApp from './components/DhanTradingApp';
+import { BrowserRouter as Router } from 'react-router-dom';
+
+function App() {
+  return (
+    <Router>
+      <DhanTradingApp />
+    </Router>
+  );
+}
+
+export default App;
